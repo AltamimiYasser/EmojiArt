3e4c34b9adb6ba12e4a526ff6b43086ca3e6540f
yasser tamimi <altamimiy89@gmail.com>
2021-12-03 15:27:05 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Yasser Tamimi on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
